96edd04a1267d566087928a234043d81dacb7501
Stas <stanislav_karashchuk@knu.ua>
2022-09-21 15:14:06 +0300

    Add file of 3_16 task

--- Lab_2/l3_16.java
+++ Lab_2/l3_16.java
@@ -4,9 +4,7 @@ public class l3_17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String temp = sc.nextLine();
-        temp = temp.replaceAll("\\s", "");
-        temp = temp.replaceAll("\\*", "");
-        System.out.println(Math.pow(Double.parseDouble(temp), 3));
-
+        System.out.println(Integer.toHexString(Integer.parseInt(temp, 16)- 1));
+        System.out.println(Integer.parseInt(temp, 16) - 1);
     }
 }
