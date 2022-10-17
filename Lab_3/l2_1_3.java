3cea74e28a0afd11b0493bd982ca7ff442806e92
staaason <72968733+staaason@users.noreply.github.com>
2022-10-17 12:38:23 +0300

    Revert "Karashchuk Stanislav третя лабораторна робота Java"

--- Lab_3/l2_1_3.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class l2_1_3 {
-    public static void main(String[] args) {
-      System.out.print("Hello, Stanislav!\n");
-      System.out.printf("Hello, %s", "Stanoslav");
-    }
-}
--- Lab_3/l2_3_1.java
+++
@@ -1,7 +0,0 @@
-public class l2_3_1 {
-    public static void main(String[] args) {
-        for(int i = 1; i < 101; ++i){
-            System.out.println(i);
-        }
-    }
-}
