18bb9c889c6102de60de1374b88b373430a82763
Stas <stanislav_karashchuk@knu.ua>
2022-10-19 13:50:29 +0300

    Add files of 5 HW

--- Lab_6/l6_1_1.java
+++
@@ -1,7 +0,0 @@
-import com.mypack.MyClass;
-public class l6_1_1 {
-    public static void main(String[] args) {
-        MyClass a = new MyClass();
-        System.out.println("Out of the package");
-    }
-}
